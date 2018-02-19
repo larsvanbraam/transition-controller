447918062854674b5af3056ee27ad7db1489aa4f
Lars van Braam <lars@mediamonks.com>
2018-02-19 22:43:10 +0100

    Export the transitionDirection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import _export from './lib/AbstractTransitionController';
 
+export { default as TransitionDirection } from './lib/enum/TransitionDirection';
 export { default as TransitionEvent } from './lib/event/TransitionEvent';
 
 export default _export;
