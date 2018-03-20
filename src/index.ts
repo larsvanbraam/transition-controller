874640326ba0649b47a09c9598b87ad93d279965
Lars van Braam <lars@mediamonks.com>
2018-03-20 14:28:10 +0100

    Expose the timeline utils

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,11 @@ import _export from './lib/AbstractTransitionController';
 
 export { default as TransitionDirection } from './lib/enum/TransitionDirection';
 export { default as TransitionEvent } from './lib/event/TransitionEvent';
+export {
+  clearTimeline,
+  killAndClearTimeline,
+  cloneTimeline,
+  createTimeline,
+} from './lib/util/TimelineUtils';
 
 export default _export;
