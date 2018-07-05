44c0d0a1af2ef5bb4d277c81b3340b009c53c007
Lars van Braam <lars@mediamonks.com>
2018-07-05 13:43:36 +0200

    Update docs Export the interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import _export from './lib/AbstractTransitionController';
 
+export {
+  default as IAbstractTransitionControllerOptions,
+} from './lib/interface/IAbstractTranstitionControllerOptions';
+export { default as ICreateTimelineOptions } from './lib/interface/ICreateTimelineOptions';
+
 export { default as TransitionDirection } from './lib/enum/TransitionDirection';
 export { default as TransitionEvent } from './lib/event/TransitionEvent';
 export {
