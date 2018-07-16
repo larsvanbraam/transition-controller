7affb094c67e399359396dcf4a7f9bcb8677da77
Lars van Braam <lars@mediamonks.com>
2018-07-16 15:40:21 +0200

    Fix wrong export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export {
 } from './lib/interface/IAbstractTranstitionControllerOptions';
 export { ICreateTimelineOptions } from './lib/interface/ICreateTimelineOptions';
 
-export { default as TimelineType } from './lib/enum/TransitionDirection';
+export { default as TimelineType } from './lib/enum/TimelineType';
 export { default as TransitionDirection } from './lib/enum/TransitionDirection';
 export { default as TransitionEvent } from './lib/event/TransitionEvent';
 export {
