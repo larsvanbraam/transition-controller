0d28ca79af7a0da59f781315b5b23dfb6702d873
Lars van Braam <lars@mediamonks.com>
2019-06-03 12:36:21 +0200

    Use the createEventClass util

--- src/lib/event/TransitionEvent.ts
+++ src/lib/event/TransitionEvent.ts
@@ -1,26 +1,13 @@
-import { AbstractEvent } from 'seng-event';
+import { createEventClass } from 'seng-event';
 
 /**
  * ### TransitionEvent
  * The transitionController uses the seng-event module to dispatch events to notify parent classes about
  * certain events. The following events can be listened to.
  */
-class TransitionEvent extends AbstractEvent {
-  public static types = {
-    TRANSITION_IN_START: 'TRANSITION_IN_START',
-    TRANSITION_IN_COMPLETE: 'TRANSITION_IN_COMPLETE',
-    TRANSITION_OUT_START: 'TRANSITION_OUT_START',
-    TRANSITION_OUT_COMPLETE: 'TRANSITION_OUT_COMPLETE',
-  };
-
-  /**
-   * The clone method returns a cloned instance of the original event.
-   *
-   * @public
-   */
-  public clone(): TransitionEvent {
-    return new TransitionEvent(this.type);
-  }
-}
-
-export default TransitionEvent;
+export default class TransitionEvent extends createEventClass()(
+  'TRANSITION_IN_START',
+  'TRANSITION_IN_COMPLETE',
+  'TRANSITION_OUT_START',
+  'TRANSITION_OUT_COMPLETE',
+) {}
