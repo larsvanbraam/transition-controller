4771dfbc099795c132fa100652f7a7f67fd75351
Lars van Braam <lars@mediamonks.com>
2020-04-24 18:58:04 +0200

    Ignore the bundle.ts in the normal build process

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,5 +1,5 @@
-// Export all classes (named and default) that you want to be publicly available
-// in the browser as named exports here.
+// These exports are used to create a webpack build that can be used as a library, you should
+// export all classes (named and default) that you want to be publicly available in the browser as named exports here.
 // Interfaces should be ignored, as they don't export any code.
 export { default as AbstractTransitionController } from './lib/AbstractTransitionController';
 export * from './lib/util/TimelineUtils';
